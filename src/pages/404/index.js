c8e698d6f1bf2b9b2435b502ee8d1f4c177bf238
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-10-10 23:13:03 -0300

    Update styled for page 404

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import "./styles.css";
 
 function page404() {
   return (
     <div className="container">
+      <Link to="/" className="linkForHome">
+        <h1>Home Page</h1>
+      </Link>
       <h2>
         Error: 404 <strong>Page not function</strong>
       </h2>
