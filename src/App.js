34710808ce105d063ddcc6a33c4431601d51dad2
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-10-10 22:26:45 -0300

    Limpando projeto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1>Deus é mais</h1>
     </div>
   );
 }
